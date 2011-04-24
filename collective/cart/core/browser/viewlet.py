acb7f8d304a8e52e479c156d9197536b50cf02c3
Taito Horiuchi <taito.horiuchi@gmail.com>
2011-04-24 21:46:34 +0300

    template -> viewlet

--- collective/cart/core/browser/viewlet.py
+++ collective/cart/core/browser/viewlet.py
@@ -145,6 +145,96 @@ class CartConfigTypesViewlet(CartViewletBase):
         return html
 
 
+class EditProductViewlet(CartViewletBase):
+
+    index = render = ViewPageTemplateFile("viewlets/edit_product.pt")
+
+    def update(self):
+        form = self.request.form
+        if form.get('form.button.UpdateProductBasic', None) is not None:
+            context = aq_inner(self.context)
+            product = IProduct(context)
+            re = getUtility(IRegularExpression)
+            price = form.get('price')
+            if re.float(price):
+                product.price = IPortal(context).decimal_price(price)
+            unlimited_stock = form.get('unlimited_stock')
+            if unlimited_stock == 'on':
+                product.unlimited_stock = True
+            if unlimited_stock != 'on':
+                product.unlimited_stock = False
+            stock = form.get('stock')
+            if re.integer(stock):
+                product.stock = int(stock)
+            max_addable_quantity = form.get('max_addable_quantity')
+            if re.integer(max_addable_quantity):
+                product.max_addable_quantity = int(max_addable_quantity)
+            return self.request.response.redirect(self.current_url) 
+
+    def fields(self):
+        context = aq_inner(self.context)
+        product = IProduct(context)
+        res = []
+        price = dict(
+            label = _(u'Price'),
+            description = 'Input Price.',
+            field = '<input type="text" name="price" id="price" value="%s" size="6" />' %product.price,
+        )
+        res.append(price)
+        unlimited_stock_field = '<input type="checkbox" name="unlimited_stock" id="unlimited_stock" value="on" />'
+        if product.unlimited_stock == True:
+            unlimited_stock_field = '<input type="checkbox" name="unlimited_stock" id="unlimited_stock" value="on" checked="checked" />'
+        unlimited_stock = dict(
+            label = _(u'Unlimited Stock'),
+            description = _(u'Check this if you have unlimited amount of stock.'),
+            field = unlimited_stock_field,
+        )
+        res.append(unlimited_stock)
+        stock = dict(
+            label = _(u'Stock'),
+            description = 'Input Stock.',
+            field = '<input type="text" name="stock" id="stock" value="%s" size="5" />' %product.stock,
+        )
+        res.append(stock)
+        max_addable_quantity = dict(
+            label = _(u'Maximum Addable Quantity'),
+            description = _('You need to specify this if you checked Unlimited Stock.'),
+            field = '<input type="text" name="max_addable_quantity" id="max_addable_quantity" value="%s" size="5" />' % product.max_addable_quantity,
+        )
+        res.append(max_addable_quantity)
+#        weight_unit = dict(
+#            label = _(u'Weight Unit'),
+#            description = _('Select Weight Unit.'),
+#            field = self.select_weight_unit(product),
+#        )
+#        res.append(weight_unit)
+#        weight =dict(
+#            label = _('Weight'),
+#            description = _('Input Weight.'),
+#            field = '<input type="text" name="weight" id="weight" value="%s" size="5" />' % product.weight,
+#        )
+#        res.append(weight)
+#        height =dict(
+#            label = _('Height'),
+#            description = _('Input Height in cm unit.'),
+#            field = '<input type="text" name="height" id="height" value="%s" size="5" />' % product.height,
+#        )
+#        res.append(height)
+#        width = dict(
+#            label = _('Width'),
+#            description = _('Input Width in cm unit.'),
+#            field = '<input type="text" name="width" id="width" value="%s" size="5" />' % product.width,
+#        )
+#        res.append(width)
+#        depth = dict(
+#            label = _('Depth'),
+#            description = _('Input Depth in cm unit.'),
+#            field = '<input type="text" name="depth" id="depth" value="%s" size="5" />' % product.depth,
+#        )
+#        res.append(depth)
+        return res
+
+
 class CartProductValuesViewlet(CartViewletBase):
     """Product Values Viewlet for Content Types."""
 
--- collective/cart/core/interfaces/viewlet_manager.py
+++ collective/cart/core/interfaces/viewlet_manager.py
@@ -1,13 +1,21 @@
 from zope.viewlet.interfaces import IViewletManager
 
+
 class ICartConfigViewletManager(IViewletManager):
     """A viewlet manager for Cart Config."""
+
+
+class IEditProductViewletManager(IViewletManager):
+    """A viewlet manager for Edit Product."""
+
 
 class ICartViewletManager(IViewletManager):
     """A viewlet manager for Cart."""
 
+
 class ICartTotalsViewletManager(IViewletManager):
     """A viewlet manager for total prices."""
+
 
 class IFixedInfoViewletManager(IViewletManager):
     """A viewlet manager for fixed infos."""
